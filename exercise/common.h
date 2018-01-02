0f8d71b31b64669290043a5622a3180317f78500
BigVan <alpc_metic@live.com>
2018-01-02 19:15:03 +0800

    201801021914

---
+++ exercise/common.h
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <list>
+#include <iostream>
+#include <queue>
+
+using namespace std;
