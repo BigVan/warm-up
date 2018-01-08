971f13fd4680588193b604a46af954bb21a130ce
BigVan <alpc_metic@live.com>
2018-01-08 20:16:23 +0800

    .

--- exercise/leetcode587.cc
+++ exercise/leetcode587.cc
@@ -13,69 +13,58 @@ class Solution {
 public:
 	vector<Point> outerTrees(vector<Point>& points) {
 
-		vector<Point> ret{};
+		set<int> ret{};
 		if (points.size() <= 2) {
-			ret.assign(points.begin(), points.end());
-			return ret;
+			return vector<Point>(points.begin(), points.end());
 		}
 		sort(points.begin(), points.end(), [](Point a, Point b){
 			if (a.y < b.y) return true; 
 			if (a.y > b.y) return false;
 			return a.x <= b.x;
 		});
-		/*for (auto it : points) {
-			cout << it.x << ' ' << it.y << endl;
-		}
-		cout << endl;*/
-		stack<int> s;
-		s.push(0);
-		s.push(1);
+			
+		vector<int> s;
+		s.push_back(0);
+		s.push_back(1);
 		for (int i = 2; i < points.size(); i++) {
-			s.push(i);
 			while (s.size() >= 2) {				
-				Point vex1(points[s.top()].x - points[s.top() - 1].x, points[s.top()].y - points[s.top() - 1].y);				
-				Point vex0(points[s.top()-1].x - points[s.top() - 2].x, points[s.top()-1].y - points[s.top() - 2].y);
-				if (vex0.x*vex1.y - vex0.y*vex1.y >= 0) {			
+				Point vex1(points[i].x - points[s.back()].x, points[i].y - points[s.back()].y);				
+				Point vex0(points[s.back()].x - points[*(s.end()-2)].x, points[s.back()].y - points[*(s.end()-2)].y);
+				if (vex0.x*vex1.y - vex0.y*vex1.x >= 0) {						
 					break;
 				}
-				s.pop();
+				s.pop_back();
 			}
-
+			s.push_back(i);
 		}
 		while (!s.empty()) {
-			ret.push_back(points[s.top()]);
-			s.pop();
+		//	ret.push_back(points[s.back()]);
+			ret.insert(s.back());
+			s.pop_back();
 		}
-		s.push(points.size() - 1);
-		s.push(points.size() - 2);
+		s.push_back(points.size() - 1);
+		s.push_back(points.size() - 2);
 		for (int i = points.size()-3; i >=0; i--) {
-			s.push(i);
 			while (s.size() >= 2) {
-
-				Point vex1(points[s.top()].x - points[s.top() - 1].x, points[s.top()].y - points[s.top() - 1].y);
-				Point vex0(points[s.top() - 1].x - points[s.top() - 2].x, points[s.top()-1].y - points[s.top() - 2].y);
-				if (vex0.x*vex1.y - vex0.y*vex1.y >= 0) {
-
+				Point vex1(points[i].x - points[s.back()].x, points[i].y - points[s.back()].y);
+				Point vex0(points[s.back()].x - points[*(s.end() - 2)].x, points[s.back()].y - points[*(s.end() - 2)].y);
+				if (vex0.x*vex1.y - vex0.y*vex1.x >= 0) {
 					break;
 				}
-				s.pop();
+				s.pop_back();
 			}
+			s.push_back(i);
 		}
-		s.pop();
+		s.pop_back();
 		while (s.size()>1) {
-			ret.push_back(points[s.top()]);
-			s.pop();
+			ret.insert(s.back());
+			s.pop_back();
 		}
-		return ret;
+		vector<Point> ans{};
+		for (auto it : ret) {
+			ans.push_back(points[it]);
+		}
+		return ans;
 	}
 };
 
-int main() {
-	vector<Point> points{ Point(1,1), Point(2,2), Point(2,0), Point(2,4), Point(3,3), Point(4,2) };
-	auto ret = Solution().outerTrees(points);
-	for (auto it : ret) {
-		cout << it.x << ' ' << it.y << endl;
-	}
-	system("pause");
-	return 0;
-}
